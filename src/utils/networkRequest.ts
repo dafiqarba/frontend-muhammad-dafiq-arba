e41a5ddc759db35c18bed6eb68e66e09a5aad011
Dafiq Arba <daviqarba@live.com>
2025-07-16 20:54:16 +0700

    Fix missing type

--- src/utils/networkRequest.ts
+++ src/utils/networkRequest.ts
@@ -6,12 +6,12 @@ export const fetchCountry = async () => {
   return await response.json()
 }
 
-export const fetchPorts = async (query: string) => {
+export const fetchPorts = async (query: string | null) => {
   const response = await fetch(`/api/proxy?path=${PORTS}${getQuery(query, 'negara')}`)
   return await response.json()
 }
 
-export const fetchGoods = async (query: string) => {
+export const fetchGoods = async (query: string | null) => {
   const response = await fetch(`/api/proxy?path=${GOODS}${getQuery(query, 'pelabuhan')}`)
   return await response.json()
 }
